34202e9316c8ccd20c80f22cd21adfeb795458d9
juntatalor <juntatalor@gmail.com>
2017-06-20 07:14:33 +0300

    Update get_count.py

--- get_count.py
+++ get_count.py
@@ -3,14 +3,12 @@ from bs4 import BeautifulSoup
 
 
 def get_count_from_price(page_num):
-    num = 0
     res_pr = []
-    for i in range(page_num):
+    for num in range(page_num, 0, 30):
         paginator = 'https://realty.domclick.ru/prodazha-kvartir/?addresses=0c5b2444-70a0-4932-980c-b4dc0d3f02b5&' \
-                    'region=0c5b2444-70a0-4932-980c-b4dc0d3f02b5&with_photo=1&offset={}'.format(str(num))
+                    'region=0c5b2444-70a0-4932-980c-b4dc0d3f02b5&with_photo=1&offset={}'.format(num)
         page = requests.get(paginator).text
         soup = BeautifulSoup(page, features='html.parser')
-        num += 30
         prices = soup.find_all(class_='offer-snippet__title')
         for pr in prices:
             sp = pr.find('span')
@@ -19,21 +17,19 @@ def get_count_from_price(page_num):
 
 
 def get_count_from_every_page(page_num):
-    num = 0
-    res_pr = []
-    count_from_page = []
-    for i in range(page_num):
+    for num in range(page_num):
         paginator = 'https://realty.domclick.ru/prodazha-kvartir/?addresses=0c5b2444-70a0-4932-980c-b4dc0d3f02b5&' \
-                    'region=0c5b2444-70a0-4932-980c-b4dc0d3f02b5&with_photo=1&offset={}'.format(str(num))
+                    'region=0c5b2444-70a0-4932-980c-b4dc0d3f02b5&with_photo=1&offset={}'.format(num)
         page = requests.get(paginator).text
         soup = BeautifulSoup(page, features='html.parser')
-        num += 30
         prices = soup.find_all(class_='offer-snippet__title')
         for pr in prices:
             sp = pr.find('span')
-            res_pr.append(int(sp.text.replace(' ', '')))
-        count_from_page.append(sum(res_pr) / len(res_pr))
-    return count_from_page
+            yeild int(sp.text.replace(' ', ''))
 
 print(get_count_from_price(1))
-print(get_count_from_every_page(2))
+
+for price in get_count_from_every_page(2):
+    print(price)
+
+prices = [p for p in get_count_from_every_page(2)]
